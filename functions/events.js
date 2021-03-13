527de5c8371a2758b17e98c44b99b52cb61fe6f6
unknown <tripathiharshcoder@gmail.com>
2021-03-13 16:24:01 +0530

    Upating contests route in Events.js

--- functions/events.js
+++ functions/events.js
@@ -1,13 +1,49 @@
-async function codechefHandler(req, res) {
-    
+let codeArray = [];
+
+const fetch = require("node-fetch");
+
+const url = `http://contesttrackerapi.herokuapp.com/`;
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const fetchData = async (platform) => {
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const dataObjects = data.result.ongoing;
+      dataObjects.map((dataObject) => {
+        const time = dataObject.EndTime;
+        const timeSplit = time.split(" ");
+        const date = new Date();
+        const day = date.getDate();
+        if (timeSplit[1] == day) {
+          if (dataObject.Platform === platform) codeArray.push(dataObject);
+        }
+        if (codeArray.length === 0)
+          codeArray.push({ Error: "No Upcoming Contest Today" });
+      });
+    })
+    .catch((e) => {
+      codeArray = [];
+    });
+};
+
+async function codechefHandler() {
+  codeArray = [];
+  await fetchData("CODECHEF");
+  return codeArray;
 }
 
 async function hackerearthHandler(req, res) {
-
+  codeArray = [];
+  await fetchData(" HACKEREARTH");
+  return codeArray;
 }
 
 async function codeforcesHandler(req, res) {
-
+  codeArray = [];
+  await fetchData("CODEFORCES");
+  return codeArray;
 }
 
-module.exports = {codechefHandler, codeforcesHandler, hackerearthHandler}
+module.exports = { codechefHandler, codeforcesHandler, hackerearthHandler };
--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
 const firebase = require("firebase/app");
-const firebaseConfig = require('./firebaseConfig')
-firebase.initializeApp(firebaseConfig);
-require("firebase/firestore");
+// const firebaseConfig = require("./firebaseConfig");
+// firebase.initializeApp(firebaseConfig);
+// require("firebase/firestore");
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("static"));
 
-const { codechefHandler, codeforcesHandler, hackerearthHandler } = require('./functions/events')
+const {
+  codechefHandler,
+  codeforcesHandler,
+  hackerearthHandler,
+} = require("./functions/events");
+const { reset } = require("nodemon");
 
 app.get("/", (req, res) => {
-    console.log('GET /')
-    res.json({ 'status': 'active' })
+  console.log("GET /");
+  res.json({ status: "active" });
 });
-  
 
-app.get('/events/codechef', codechefHandler)
-app.get('/events/hackerearth', hackerearthHandler)
-app.get('/events/codeforces', codeforcesHandler)
-
-
-
+app.get("/events/codeforces", async (req, res) => {
+  res.send(await codeforcesHandler());
+});
+app.get("/events/hackerearth", async (req, res) => {
+  res.send(await hackerearthHandler());
+});
+app.get("/events/codechef", async (req, res) => {
+  res.send(await codechefHandler());
+});
 
 // --- LISTEN TO REQUESTS ---
 const listener = app.listen(process.env.PORT || 5000, () => {
-    console.log(">> Listening on PORT " + listener.address().port);
-  });
+  console.log(">> Listening on PORT " + listener.address().port);
+});
 
-
+listener;
